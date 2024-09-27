5c50b53e67463ad20dbc87e1876e06039fc6030e
Patricia A. da Silva <patriciaananias1983@hotmail.com>
2024-09-27 17:19:25 -0300

    Update sobre.php

--- sobre.php
+++ sobre.php
@@ -1,8 +1,42 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Bem vindos ao odango's place <3</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+        <div id='container'>
+        <nav>
+            <ul class="menu">
+                <li> <a href="sobre.php"><img src="image/botao1.png" alt="sobre"  height="10px" width="10px"/>Sobre</a></li>
+                <li> <a href="curriculo.php"><img src="image/botao2.png" alt="currículo"  height="10px" width="10px"/>Currículo</a></li>
+                <li> <a href="redes.php"><img src="image/botao3.png" alt="Redes Sociais"  height="10px" width="10px"/> Minhas redes</a></li>
+                <li><a href="portifolio.php"><img src="image/botao4.png" alt="Portifólio"  height="10px" width="10px"/>Portifólio</a></li>
+                
+            </ul>
+        </nav>
+        
+        </div>
+        <footer>
+            <p>(C)2021  By Patricia Silva (PathyOdango83) |  Imagens usadas nesse site tem seus direitos autorais de seus criadores(PngTree e ColiseuGeek). </p><br>
+            <p>(C)2021 By Patricia Silva (PathyOdango83) | Images used on this site are copyrighted by their creators (PngTree and ColiseuGeek)</p><br>
+            <ul class="feet">
+                <li><a href="https://www.coliseugeek.com.br/baixar-wallpaper-fortnite-para-celular-e-pc-em-4k/"> Coliseu Geek | </a></li>
+                <li> <a href="https://pngtree.com/so/conjunto">Png tree</a></li>
+           
+            </ul>
+        </footer>
+   
+    
+    </body>
+</html>
